487d54d45647859987ef4e077347f5cdd2e127aa
José <veganum@hotmail.com>
2020-02-12 16:12:44 +0100

    subiendo proyecto

---
+++ src/app/app.module.ts
@@ -0,0 +1,44 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { routing, appRoutingProviders } from './app.routing';
+
+import { AppComponent } from './app.component';
+import { MiComponente } from './components/mi-componente/mi-componente.component';
+import { PeliculasComponent } from './components/peliculas/peliculas.component';
+import { PruebasComponent } from './components/pruebas/pruebas.component';
+import { HeaderComponent } from './components/header/header.component';
+import { SliderComponent } from './components/slider/slider.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HomrComponent } from './components/homr/homr.component';
+import { HomeComponent } from './components/home/home.component';
+import { BlogComponent } from './components/blog/blog.component';
+import { FormularioComponent } from './components/formulario/formulario.component';
+import { PaginaComponent } from './components/pagina/pagina.component';
+import { ErrorComponent } from './components/error/error.component'; 
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MiComponente,
+    PeliculasComponent,
+    PruebasComponent,
+    HeaderComponent,
+    SliderComponent,
+    SidebarComponent,
+    FooterComponent,
+    HomrComponent,
+    HomeComponent,
+    BlogComponent,
+    FormularioComponent,
+    PaginaComponent,
+    ErrorComponent
+  ],
+  imports: [
+    BrowserModule,
+    routing
+  ],
+  providers: [appRoutingProviders],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/models/pelicula.ts
@@ -0,0 +1,20 @@
+export class Pelicula{
+    /*
+    public title: string;
+    public year:  number;
+    public image: string;
+
+    constructor(title, year, image){
+        this.title= title;
+        this.year = year;
+        this.image =image;
+    }
+    */
+    constructor(
+        public title: string,
+        public year:  number,
+        public image: string
+    ){}
+
+
+}
